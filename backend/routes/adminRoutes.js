203079bd293768fe9e68ffcbc8d2739837c6e1d1
Fernando Martínez Ortiz <fermtz020700@hotmail.com>
2021-11-29 21:34:37 -0600

    added post and get routes for editProduct and deleteProduct

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -9,4 +9,32 @@ router.get('/', async function (req, res){
     res.send(admins)
 })
 
+router.post('/newProduct', async function(req, res){
+    var product = new Product(req.body);
+    await product.save()
+    res.send(product);
+})
+
+router.get('/editProduct/:id', async function (req, res){
+    var product = await Product.findById(req.params.id)
+    res.send(product)
+})
+
+router.post('/editProduct/:id', async function (req, res){
+    var id = req.params.id
+    await Product.updateOne({_id: id}, req.body)
+    res.redirect('/');
+})
+
+router.get('/delProduct/:id', async function (req, res){
+    var product = await Product.findById(req.params.id)
+    res.send(product)
+})
+
+router.post('/delProduct/:id', async function (req, res){
+    var id = req.params.id
+    await Product.remove({_id: id})
+    res.redirect('/')
+})
+
 module.exports = router;
