0391811854371e35f325f09f7cfdc378ec892221
Fernando Martínez Ortiz <fermtz020700@hotmail.com>
2021-12-01 17:54:05 -0600

    added authentication, tested new Product, added verification

---
+++ backend/middleware/verifyAccess.js
@@ -0,0 +1,34 @@
+const jwt = require('jsonwebtoken')
+
+function verifyToken(req,res,next) {
+
+    const token = req.cookies.token || '' 
+
+    console.log(token)
+
+    if (!token) {
+        return res.redirect('/login')
+    }
+    // Validar el token 
+    else {
+
+        jwt.verify(token, process.env.SECRET, function(err, data){
+
+            if (err){
+                console.log(err)
+                return res.redirect('/login')
+            }
+            else {
+            
+                req.userId = data.id
+                console.log(data)
+                next()
+            }
+
+        })
+
+    }
+
+}
+
+module.exports = verifyToken
