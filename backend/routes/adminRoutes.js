52c36611fb27f0287ba67dae44806840b1a5a6d4
MemazoG <memogarcia246@gmail.com>
2021-12-02 16:09:42 -0600

    cambio de res.redirect a res.status.json

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -61,7 +61,7 @@ router.get('/delProduct/:id', async function (req, res){
 router.post('/delProduct/:id', async function (req, res){
     var id = req.params.id
     await Product.remove({_id: id})
-    res.redirect('/')
+    res.redirect('/') 
 })
 
 module.exports = router;
