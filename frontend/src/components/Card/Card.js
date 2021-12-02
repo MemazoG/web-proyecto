061629275e80b888ad39635679505dfd4a8c7679
MemazoG <memogarcia246@gmail.com>
2021-12-01 23:47:04 -0600

    funcionalidad de algunas partes de la pagina con el back

--- frontend/src/components/Card/Card.js
+++ frontend/src/components/Card/Card.js
@@ -1,15 +1,25 @@
+import { Link } from 'react-router-dom';
 import Apple from './apple.png';
 import './Card.css';
 
-const Card = ({ name, category }) => {
+
+
+const Card = ({ name, category, prodID }) => {
+
     return (
-        <div className="tc logo-green-bg dib flex-wrap br3 pa3 ma2 grow bw2 shadow-5 pointer card">
-            <img className="img" alt="product" src={Apple}/>
-            <div>
-                <h2 className="f4">{name}</h2>
-                <p className="f5 i">{category}</p>
-            </div>
-        </div>
+        <Link 
+            to={{ 
+                    pathname:`/pagina-producto/${prodID}`, 
+                    id: { productID: prodID } 
+                }} 
+            className="tc logo-green-bg dib flex-wrap br3 pa3 ma2 grow bw2 shadow-5 pointer card" 
+            style={{ textDecoration: 'none' }}>
+                <img className="img" alt="product" src={Apple}/>
+                <div>
+                    <h2 className="f4 black-90">{name}</h2>
+                    <p className="f5 i black-90">{category}</p>
+                </div>
+        </Link>
     )
 }
 
