13e120cd344a77bcc09eac77b019b2bb5fb5ea25
MemazoG <memogarcia246@gmail.com>
2021-12-02 14:13:32 -0600

    funcionalidad agregar-producto

--- frontend/src/components/EliminarProducto/EliminarProducto.js
+++ frontend/src/components/EliminarProducto/EliminarProducto.js
@@ -1,25 +1,56 @@
-import React from 'react'
+import { React, useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useParams } from 'react-router';
 import Apple from './apple.png';
 
-const EliminarProducto = () => {
+function EliminarProducto() {
+    const emptyProduct = {
+        name: "",
+        category: "",
+        price: "",
+        description: "",
+        image: null
+    }
+    const { id } = useParams();
+    let [product, setProduct] = useState(emptyProduct);
+
+    useEffect(() => {
+        fetch(`http://localhost:9000/api/${id}`)
+            .then(res => res.json())
+            .then(response => {
+                setProduct(product = response[0])
+                console.log(product)
+            })
+            .catch(err => console.log("Error, producto no encontrado", err))
+    }, []);
+
     return (
         <div>
             <div className="flex justify-center w-100">
                 <div className="ba b--white bw2 w-30 tc">
+                    {/* IMAGEN */}
                     <img className="img" alt="product" src={Apple} />
                 </div>
 
                 <div className="ba b--yellow bw2 w-50">
-                    <h1>Nombre Producto</h1>
+                    {/* NOMBRE */}
+                    <h1>{product.name}</h1>
                     <div className="flex inline-flex ml2">
-                        <p className="ma0 w-50">Precio</p>
-                        <p className="ma0 w-50 i">Categoría</p>
+                        {/* PRECIO */}
+                        <p className="ma0 w-50">{product.price}</p>
+                        {/* CATEGORÍA */}
+                        <p className="ma0 w-50 i">{product.category}</p>
                     </div>
-                    <p>Descripción</p>
+                    {/* DESCRIPCIÓN */}
+                    <p>{product.description}</p>
                 </div>
             </div>
-            <div className="tc">
-                <button className="f3 grow no-underline br-pill ph3 pv2 ma2 dib black logo-green-bg">Regresar</button>
+            <div className="tc flex justify-center">
+                <div>
+                    <Link to="/dashboard">
+                        <button className="f3 grow no-underline br-pill ph3 pv2 ma2 dib black logo-green-bg">Regresar</button>
+                    </Link>
+                </div>
                 <button className="f3 grow no-underline br-pill ph3 pv2 ma2 dib black logo-green-bg">Eliminar</button>
             </div>
         </div>
