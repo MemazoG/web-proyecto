34190e5cd52db6f3a9c45e56d138741cfc0594cd
Fernando Martínez Ortiz <fermtz020700@hotmail.com>
2021-12-02 11:46:38 -0600

    verifyAcces envia el codigo de estatus

--- backend/middleware/verifyAccess.js
+++ backend/middleware/verifyAccess.js
@@ -2,32 +2,25 @@ const jwt = require('jsonwebtoken')
 
 function verifyToken(req,res,next) {
 
-    const token = req.cookies.token || '' 
-
+    const token = req.cookies.token || ''
     console.log(token)
-
     if (!token) {
         console.log("NO TOKEN")
-        return res.redirect('/login')
+        return res.status(401).send("Acceso no autorizado")
     }
     // Validar el token 
     else {
-
         jwt.verify(token, process.env.SECRET, function(err, data){
-
             if (err){
                 console.log(err)
-                return res.redirect('/login')
+                return res.status(400).send("Token invalido")
             }
             else {
-            
                 req.userId = data.id
                 console.log(data)
                 next()
             }
-
         })
-
     }
 
 }
